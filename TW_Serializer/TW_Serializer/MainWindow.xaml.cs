ee490d119d6e23c1cce3a43d8991f389a40a6f25
Ba Matrai <matrai.barnabas1@gmail.com>
2019-11-11 14:18:36 +0100

    start work

---
+++ TW_Serializer/TW_Serializer/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TW_Serializer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            int maximumPersonNumber = 100;
+            Person person = new Person(Name.Text, Address.Text, Phone.Text);
+
+            if (Person.SerialNumber < maximumPersonNumber)
+            {
+                string fileName = @"\person" + Person.SerialNumber + ".dat";
+                string directoryPath = @"C:\Users\matra\OneDrive\Asztali gép\codecool\TW_assignments\3rd_week\TW_Serializer";
+                string filePath = directoryPath + fileName;
+                person.Serialize(filePath);
+            }
+            else
+            {
+                MessageBox.Show("You already have 99 people, calm down!");
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string directoryPath = @"C:\Users\matra\OneDrive\Asztali gép\codecool\TW_assignments\3rd_week\TW_Serializer";
+            string fileName = @"\person1.dat";
+            string filePath = directoryPath + fileName;
+            if (File.Exists(filePath))
+            {
+                Person person = Person.Deserialize(filePath);
+                Name.Text = person.Name;
+                Address.Text = person.Address;
+                Phone.Text = person.PhoneNumber;
+            }
+        }
+    }
+}
---
+++ TW_Serializer/TW_Serializer/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TW_Serializer
+{
+    [Serializable]
+    class Person
+    {
+        private string name;
+        private string address;
+        private string phoneNumber;
+        private DateTime dataRecordingDate;
+        [NonSerialized]
+        private static int serialNumber = 0;
+
+        public Person(string name, string address, string phoneNumber)
+        {
+            this.name = name;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+            dataRecordingDate = DateTime.Now;
+            serialNumber++;
+        }
+
+        public void Serialize(string filePath)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            formatter.Serialize(stream, this);
+            stream.Close();
+        }
+
+        public static Person Deserialize(string filePath)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            Person person = (Person)formatter.Deserialize(stream);
+            stream.Close();
+
+            return person;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+        }
+
+        public static int SerialNumber
+        {
+            get { return serialNumber; }
+        }
+    }
+}
